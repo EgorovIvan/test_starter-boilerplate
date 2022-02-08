b7e04320da2f75e736994f3a00fefedd9d9f69fe
geek <sportsmenq@mail.ru>
2022-02-08 12:03:50 +0500

    start test

---
+++ build/static/js/12.c778fedd.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpemilus=this.webpackJsonpemilus||[]).push([[12],{575:function(e,s,n){"use strict";n.r(s);var t=n(3);n(0);s.default=function(){return Object(t.jsx)("div",{children:"Home component works!"})}}}]);
+//# sourceMappingURL=12.c778fedd.chunk.js.map
